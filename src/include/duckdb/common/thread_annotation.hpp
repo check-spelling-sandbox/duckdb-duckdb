311a1fac648eee6099f26e25c30ca5b80662133a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-06-23 19:30:54 -0400

    spelling: annotation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/include/duckdb/common/thread_annotation.hpp
+++ src/include/duckdb/common/thread_annotation.hpp
@@ -6,7 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-// Thread annotatio which enables clang thread safety analysis, a C++ language extension which warns about potential
+// Thread annotation which enables clang thread safety analysis, a C++ language extension which warns about potential
 // race conditions in code. The analysis is completely static (i.e. compile-time); there is no run-time overhead. For
 // more information, please refer to: https://clang.llvm.org/docs/ThreadSafetyAnalysis.html#reference-guide
 

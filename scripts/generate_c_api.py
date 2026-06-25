2f978f5fec7ba0ae830d358a044555cfe0454ab5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-06-25 18:42:36 -0400

    spelling: also, please, add
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- scripts/generate_c_api.py
+++ scripts/generate_c_api.py
@@ -202,7 +202,7 @@ def parse_capi_function_definitions():
 
     if len(function_groups) != len(ORIGINAL_FUNCTION_GROUP_ORDER):
         print(
-            "The list used to match the original order of function groups in the original the duckdb.h file does not match the new one. Did you add a new function group? please also add it to ORIGINAL_FUNCTION_GROUP_ORDER for now."
+            "The list used to match the original order of function groups in the original the duckdb.h file does not match the new one. Did you add a new function group? Also, please, add it to ORIGINAL_FUNCTION_GROUP_ORDER for now."
         )
 
     for order_group in ORIGINAL_FUNCTION_GROUP_ORDER:
